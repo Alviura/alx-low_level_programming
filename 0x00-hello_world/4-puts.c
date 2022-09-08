677188e1f29abaac4d244e418411911405c64a49
Alvin Mwaura <mwauralvin07@gmail.com>
2022-09-08 12:03:33 -0700

    changed file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/*
- * the function prints a string
+/**
+ * main function prints the string
+ * Return: 0 if brings the expected output, non-zero otherwise
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
