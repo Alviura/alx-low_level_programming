a4761d29a5fa296d06a82a44dd74b7d696fc4ffc
Alvin Mwaura <mwauralvin07@gmail.com>
2022-09-13 01:47:30 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a newline.
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c<='z'; j++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+	
