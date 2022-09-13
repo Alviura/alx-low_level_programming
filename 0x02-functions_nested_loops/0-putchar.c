9c9265d062e876b8375a815047d94a4f79f94ef3
Alvin Mwaura <mwauralvin07@gmail.com>
2022-09-13 00:33:26 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar,
+ * followd by a newline.
+ * Return: Always 0 
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+File 0-putchar.c is a C program that prints putchar,followed by a newline.
