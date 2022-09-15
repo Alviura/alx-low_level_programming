3648a6b5553dad11c6f3d95e281a6e22e3c637e4
Alvin Mwaura <mwauralvin07@gmail.com>
2022-09-15 12:31:03 -0700

    Changed task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_most_numbers - prints the numbers, from 0 to 9,
- * except 2 and 4,
+ * except 2 and 4, followed by a new line
  */
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 10; i++)
 	{
-		if((i == 50) || (i == 52))
+		if (i != 2 && i != 4)
 		{
-			continue;
+			putchar(i + '0');
 		}
-		putchar(i);
 	}
-	putchar(10);
+
+	putchar('\n');
 }
