1bcbfbc4681b3c73e3ae86571f1e1ec5d6cb4066
Alviura <alviura@outlook.com>
2023-06-20 03:08:25 -0700

    Prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,13 +14,13 @@ void print_alphabet_x10(void)
 	int alphabet;
 	int times;
 
-	for (times = 0; times < 10; times ++)
+	for (times = 0; times < 10; times++)
 	{
-		for (alphabet = 97; alphabet <=122; alphabet++)
+		for (alphabet = 97; alphabet <= 122; alphabet++)
 		{
 			_putchar(alphabet);
 		}
-	        _putchar('\n');
+		_putchar('\n');
 	}
 
 }
