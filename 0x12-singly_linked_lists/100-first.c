69c6bf2a38ab35ddee4325af2297dbd537be6504
Alviura <alviura@outlook.com>
2023-07-27 09:26:34 -0700

    A function that prints a string brfore the main funtion is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * _printf - prints the statement below
+ * Return: Always 0
+ */
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int _printf(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+	return (0);
+}
+
+int main(void)
+{
+	_printf();
+	printf("(A tortoise, having pretty good sense of a hare's nature,
+       		challenges one to a race.)\n");
+	return (0);
+}
